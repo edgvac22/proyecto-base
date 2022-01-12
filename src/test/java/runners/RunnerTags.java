a203f5263fd0c39201575472a8066427e3805327
edgvac2298 <edgardovac2298@gmail.com>
2022-01-12 00:33:16 -0500

    First Feature

--- src/test/java/runners/RunnerTags.java
+++ src/test/java/runners/RunnerTags.java
@@ -6,7 +6,8 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions (features = "src/test/resources/features",
+@CucumberOptions (
+                              features = "src/test/resources/features/academyChoucair.feature",
                               tags = "@Regresion",
                               glue = " co.com.choucair.certification.proyectobase.stepdefinitions",
                               snippets = SnippetType.CAMELCASE)
