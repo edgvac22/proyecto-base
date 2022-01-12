46107188c51e9526c39573e96b4271af1f45e5b9
edgvac2298 <edgardovac2298@gmail.com>
2022-01-12 13:48:48 -0500

    Interacting with objects

--- src/main/java/tasks/Login.java
+++ src/main/java/tasks/Login.java
@@ -3,6 +3,9 @@ package tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import userinterface.ChoucairLoginPage;
 
 public class Login implements Task {
     public static Login onThePage() {
@@ -11,6 +14,10 @@ public class Login implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-
+        actor.attemptsTo(Click.on(ChoucairLoginPage.LOGGIN_BUTTON),
+        Enter.theValue("IngresaTuUsuario").into(ChoucairLoginPage.INPUT_USER),
+        Enter.theValue("IngresaTuContraseña").into(ChoucairLoginPage.INPUT_PASSWORD),
+        Click.on(ChoucairLoginPage.ENTER_BUTTON)
+    );
     }
 }
--- src/main/java/userinterface/ChoucairLoginPage.java
+++ src/main/java/userinterface/ChoucairLoginPage.java
@@ -8,4 +8,5 @@ public class ChoucairLoginPage extends PageObject {
     public static final Target LOGGIN_BUTTON = Target.the("button that shows us the form to login").located(By.xpath("//div[@class='d-none d-lg-block']//strong[contains(text), 'Ingresar')]"));
     public static final Target INPUT_USER = Target.the("where do we write the user").located(By.id("username"));
     public static final Target INPUT_PASSWORD = Target.the("where do we write the password").located(By.id("password"));
+    public static final Target ENTER_BUTTON = Target.the("button to confirm login").located(By.xpath("//button[contains(@class, 'btn btn-primary')]"));
 }
