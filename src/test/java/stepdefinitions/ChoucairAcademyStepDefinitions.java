118d910d9461e877ed8ad7e2d8a180870cced0b4
edgvac2298 <edgardovac2298@gmail.com>
2022-01-12 00:45:34 -0500

    Adding Method setStage

--- src/test/java/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ src/test/java/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -1,10 +1,17 @@
 package stepdefinitions;
-
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
 
 public class ChoucairAcademyStepDefinitions {
+    @Before
+    public void setStage (){
+        OnStage.setTheStage(new OnlineCast());
+    }
+
     @Given("^than brandon wants to learn automation at the academy Choucair$")
     public void thanBrandonWantsToLearnAutomationAtTheAcademyChoucair(){
 
